87821e25de8375e7ee67fdf9b0b593a41f398444
Habibun Noby <habibun.noby@gmail.com>
2021-10-24 20:00:14 +0600

    chore: refactor cmd

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
-php bin/console rabbitmq:setup-fabric
 docker pull rabbitmq:3-management
 docker run --rm -it -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+php bin/console rabbitmq:setup-fabric
